09b9f6581a5989613bb2febf866171cf5e22b584
Vlad1slav <Vvlad1slavV@yandex.ru>
2020-02-21 15:20:08 +0300

    control_1

--- src/com/itmo/r3135/Control.java
+++ src/com/itmo/r3135/Control.java
@@ -1,11 +1,25 @@
 package com.itmo.r3135;
 
+import java.io.IOException;
+
 public class Control {
 
 
     //control methods
 
     public void help() {
+        System.out.printf("%-25s%5s%n","add {element}", "Добавить новый элемент в коллекцию");
+        System.out.printf("%-25s%5s%n","update id {element}", "Обновить значение элемента коллекции, id которого равен заданному");
+        System.out.printf("%-25s%5s%n","remove_greater {element}", "Удалить из коллекции все элементы, превышающие заданный");
+        System.out.printf("%-25s%5s%n","add_if_min {element}", "Добавить новый элемент в коллекцию, если его значение меньше, чем у наименьшего элемента этой коллекции");
+        System.out.printf("%-25s%5s%n","remove_by_id id", "Удалить элемент из коллекции по его id");
+        System.out.printf("%-25s%5s%n","execute_script file_name", "Считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.");
+        System.out.printf("%-30s%5s%n","group_counting_by_coordinate", "Сгруппировать элементы коллекции по значению поля coordinates, вывести количество элементов в каждой группе\n");
+        System.out.printf("%-25s%5s%n","filter_contains_name name", "Вывести элементы, значение поля name которых содержит заданную подстроку");
+        System.out.printf("%-25s%5s%n","print_field_descending_price price", "вывести значения поля price в порядке убывания");
+        System.out.printf("%-25s%5s%n","clear", "Очистить коллекцию");
+        System.out.printf("%-25s%5s%n","save", "Сохранить коллекцию в файл");
+        System.out.printf("%-25s%5s%n","exit", "Завершить программу (без сохранения в файл)");
     }
 
     public void show() {
@@ -49,6 +63,10 @@ public class Control {
 
     public void info() {
     }
+    public void load_collection() throws IOException{
+
+    }
+
 
 
 }
